3f6c46ed0b961a6934b030ed7fa84a50b54c9d8a
Jan Airick C. Indefonso <83742031+Radorikiri@users.noreply.github.com>
2022-03-14 16:14:37 +0800

    altered glDrawArrays and color

--- CS0045/March142022/activity.cpp
+++ CS0045/March142022/activity.cpp
@@ -36,16 +36,17 @@ glBindBuffer(GL_ARRAY_BUFFER, VBOid);
 glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*18, polygonvertices, GL_STATIC_DRAW);
 glEnableClientState(GL_VERTEX_ARRAY);
 glVertexPointer(3, GL_FLOAT, 0, 0);
-glDrawArrays(GL_POLYGON,0,18);
+glDrawArrays(GL_LINE_LOOP,0,6);
 glDisableClientState(GL_VERTEX_ARRAY);
 //bind with 0, so, switch back to normal pointer operation
 glBindBuffer(GL_ARRAY_BUFFER, 0);
 glDeleteBuffers(1, &VBOid);
-glLineWidth(6);
 }
 void display(){
+glClearColor(0,0,1,0);
 glClear(GL_COLOR_BUFFER_BIT);
-glColor4f(.20f,.25f,.08f,.09f);
+glLineWidth(6);
+glColor4f(1.0f,0.0f,1.0f,0.0f);
 polygon();
 glFlush();
 }

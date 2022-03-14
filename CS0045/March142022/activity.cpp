ee3be7570a5115c3daa943348f0775304cf709fe
Jan Airick C. Indefonso <83742031+Radorikiri@users.noreply.github.com>
2022-03-14 16:12:07 +0800

    altered polygonvertices

--- CS0045/March142022/activity.cpp
+++ CS0045/March142022/activity.cpp
@@ -5,14 +5,6 @@
 using namespace std;
 void display();
 
-//GLfloat polygonvertices[] = {
-//-0.5f,0.5f,0.0f, //first vertex
-//0.5f,0.5f,0.0f, //second vertex
-//1.0f,0.0f,0.0f, //third vertex
-//0.5f,-0.5f,0.0f, //fourth vertex
-//-0.5f,-0.5f,0.0f, //fifth vertex
-//-1.0f,0.0f,0.0f //sixth vertex
-//};
 int main(int argc, char** argv){
 glutInit(&argc, argv);
 glutInitWindowSize(800, 600);
@@ -24,25 +16,32 @@ glutMainLoop();
 }
 }
 void polygon(){
-	glBegin(GL_LINE_LOOP);
-	glVertex3f(-0.5f,0.5f,0.0f);
-	glVertex3f(0.5f,0.5f,0.0f);
-	glVertex3f(1.0f,0.0f,0.0f,);
+	//glBegin(GL_LINE_LOOP);
+	//glVertex3f(-0.5f,0.5f,0.0f);
+	//glVertex3f(0.5f,0.5f,0.0f);
+	//glVertex3f(1.0f,0.0f,0.0f,);
 	//insert vertex here
+	GLfloat polygonvertices[] = {
+	       -0.5f,0.5f,0.0f, //first vertex
+		0.5f,0.5f,0.0f, //second vertex
+		1.0f,0.0f,0.0f, //third vertex
+		0.5f,-0.5f,0.0f, //fourth vertex
+	       -0.5f,-0.5f,0.0f, //fifth vertex
+	      -1.0f,0.0f,0.0f //sixth vertex
+};
 	
-	glLineWidth(6);
-	glEnd();
-//GLuint VBOid;
-//glGenBuffers(1, &VBOid);
-//glBindBuffer(GL_ARRAY_BUFFER, VBOid);
-//glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*18, polygonvertices, GL_STATIC_DRAW);
-//glEnableClientState(GL_VERTEX_ARRAY);
-//glVertexPointer(3, GL_FLOAT, 0, 0);
-//glDrawArrays(GL_POLYGON,0,18);
-//glDisableClientState(GL_VERTEX_ARRAY);
-//// bind with 0, so, switch back to normal pointer operation
-//glBindBuffer(GL_ARRAY_BUFFER, 0);
-//glDeleteBuffers(1, &VBOid);
+GLuint VBOid;
+glGenBuffers(1, &VBOid);
+glBindBuffer(GL_ARRAY_BUFFER, VBOid);
+glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*18, polygonvertices, GL_STATIC_DRAW);
+glEnableClientState(GL_VERTEX_ARRAY);
+glVertexPointer(3, GL_FLOAT, 0, 0);
+glDrawArrays(GL_POLYGON,0,18);
+glDisableClientState(GL_VERTEX_ARRAY);
+//bind with 0, so, switch back to normal pointer operation
+glBindBuffer(GL_ARRAY_BUFFER, 0);
+glDeleteBuffers(1, &VBOid);
+glLineWidth(6);
 }
 void display(){
 glClear(GL_COLOR_BUFFER_BIT);
